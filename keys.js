2aebea08d59cb7ba53cf82a428d607500fd63ec5
Manshi Malik <140046353+manshi8923@users.noreply.github.com>
2023-12-15 17:09:40 +0530

    m

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoUrl:"mongodb+srv://manshimalik456:g1bwsiGR4e0l3TqT@cluster0.ltbcdov.mongodb.net/insta",
+    mongoUrl:"mongodb+srv://manshimalik456:g1bwsiGR4e0l3TqT@cluster0.ltbcdov.mongodb.net/quantrade",
     Jwt_secret: "faslkfocvneofu"
 };
